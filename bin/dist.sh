8603eac53e05fc5aa6ff491d8f05ba7dbb23a9b4
janpapenbrock <j.papenbrock@hochzehn.com>
2017-07-23 13:43:40 +0200

    Re-structure dist files to make different logos discoverable better.

--- bin/dist.sh
+++ bin/dist.sh
@@ -4,17 +4,29 @@ SRC_DIR=src
 DIST_DIR=dist
 
 mkdir -p "$DIST_DIR"
-rm "$DIST_DIR"/*
+
+if [ -z "$DIST_DIR" ]; then
+  echo "Error: Empty DIST_DIR, exiting before attempting rm -rf"
+  exit 99
+else
+  rm -rf "$DIST_DIR"/*
+fi
 
 function createLogoVersions() {
   LOGO_TYPE=$1
 
   echo "Generating dist files for '$LOGO_TYPE'"
 
-  SRC_SVG="$SRC_DIR"/"$LOGO_TYPE".min.svg
-  TARGET_SVG="$DIST_DIR"/"$LOGO_TYPE".svg
+  TARGET_DIR="$DIST_DIR/$LOGO_TYPE"
 
+  mkdir -p "$TARGET_DIR"
+
+  SRC_SVG="$SRC_DIR"/"$LOGO_TYPE".min.svg
+
+  echo -n "  Generating SVG version..."
+  TARGET_SVG="$TARGET_DIR"/"$LOGO_TYPE".svg
   cp "$SRC_SVG" "$TARGET_SVG"
+  echo "OK"
 
   echo "  Generating PNG versions..."
   resize 64
@@ -28,8 +40,8 @@ function createLogoVersions() {
 function resize() {
   size=$1
   density=$((size * 4))
-  echo -n "    $size"x"$size""..."
-  docker run --rm --volume "$PWD/$SRC_SVG":"/app/input.svg" --volume "$PWD/$DIST_DIR":"/output" --workdir "/app" acleancoder/imagemagick-full convert -background none -resize "$size"x -density "$density" -depth 8 input.svg /output/"$LOGO_TYPE"-"$size".png
+  echo -n "    $size"x...
+  docker run --rm --volume "$PWD/$SRC_SVG":"/app/input.svg" --volume "$PWD/$TARGET_DIR":"/output" --workdir "/app" acleancoder/imagemagick-full convert -background none -resize "$size"x -density "$density" -depth 8 input.svg /output/"$LOGO_TYPE"-"$size".png
   echo "OK"
 }
 
@@ -37,17 +49,17 @@ function optimizePng() {
   DIR=$1
 
   echo "Size before optimization"
-  du -h "$DIR"
+  du -h --max-depth=0 "$DIR"
 
   echo -n "Optimizing..."
   CURRENT="$PWD"
   cd "$DIR"
-  docker run --rm -v "$PWD":/source buffcode/docker-optipng -q -o7 *.png
+  docker run --rm -v "$PWD":/source buffcode/docker-optipng -q -o7 **/*.png
   cd "$CURRENT"
   echo "OK"
 
   echo "Size after optimization"
-  du -h "$DIR"
+  du -h --max-depth=0 "$DIR"
 }
 
 createLogoVersions "arrow"
