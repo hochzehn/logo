a6633bed217e818cc567696a67a6756e2dce030e
janpapenbrock <j.papenbrock@hochzehn.com>
2017-07-23 11:59:38 +0200

    Refactor dist script to allow multiple files more easily.

--- bin/dist.sh
+++ bin/dist.sh
@@ -3,39 +3,55 @@
 SRC_DIR=src
 DIST_DIR=dist
 
-SRC_SVG="$SRC_DIR"/logo.svg
-
 mkdir -p "$DIST_DIR"
 rm "$DIST_DIR"/*
 
-cp "$SRC_DIR"/logo.min.svg "$DIST_DIR"/logo.svg
+function createLogoVersions() {
+  LOGO_TYPE=$1
 
-function resize() {
-    size=$1
-    density=$((size * 4))
-    echo -n "$size"x"$size""..."
-    docker run --rm --volume "$PWD/$SRC_SVG":"/app/input.svg" --volume "$PWD/$DIST_DIR":"/output" --workdir "/app" acleancoder/imagemagick-full convert -background none -resize "$size"x -density "$density" -depth 8 input.svg /output/logo-"$size".png
-    echo "OK"
+  echo "Generating dist files for '$LOGO_TYPE'"
+
+  SRC_SVG="$SRC_DIR"/"$LOGO_TYPE".min.svg
+  TARGET_SVG="$DIST_DIR"/"$LOGO_TYPE".svg
+
+  cp "$SRC_SVG" "$TARGET_SVG"
+
+  echo "  Generating PNG versions..."
+  resize 64
+  resize 196
+  resize 256
+  resize 512
+
+  echo ""
 }
 
-echo "Generating PNG versions..."
-resize 64
-resize 196
-resize 256
-resize 512
+function resize() {
+  size=$1
+  density=$((size * 4))
+  echo -n "    $size"x"$size""..."
+  docker run --rm --volume "$PWD/$SRC_SVG":"/app/input.svg" --volume "$PWD/$DIST_DIR":"/output" --workdir "/app" acleancoder/imagemagick-full convert -background none -resize "$size"x -density "$density" -depth 8 input.svg /output/"$LOGO_TYPE"-"$size".png
+  echo "OK"
+}
 
-echo ""
-echo "Size before optimization"
-du -h "$DIST_DIR"
+function optimizePng() {
+  DIR=$1
 
-echo -n "Optimizing..."
-CURRENT="$PWD"
-cd "$DIST_DIR"
-docker run --rm -v "$PWD":/source buffcode/docker-optipng -q -o7 *.png
-cd "$CURRENT"
-echo "OK"
+  echo "Size before optimization"
+  du -h "$DIR"
 
-echo "Size after optimization"
-du -h "$DIST_DIR"
+  echo -n "Optimizing..."
+  CURRENT="$PWD"
+  cd "$DIR"
+  docker run --rm -v "$PWD":/source buffcode/docker-optipng -q -o7 *.png
+  cd "$CURRENT"
+  echo "OK"
+
+  echo "Size after optimization"
+  du -h "$DIR"
+}
+
+createLogoVersions "logo"
+
+optimizePng "$DIST_DIR"
 
 bin/own.sh "$PWD/$DIST_DIR"
